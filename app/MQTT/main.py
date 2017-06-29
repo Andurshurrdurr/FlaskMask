a073c78e900759834e3063293e20efedb9ec6082
Anders <andershurum@github.com>
2017-06-29 10:42:53 +0200

    removed become pass prompt

--- app/MQTT/main.py
+++ app/MQTT/main.py
@@ -1,17 +1,49 @@
 import paho.mqtt.client as mqtt
+import json
 
-# The callback for when the client receives a CONNACK response from the server.
+
+from model.model_devices import create_reading
+
+
+def parsedata(data):
+    """This funcion parses the data for inserting to the database"""
+    try:
+        # First we get device id
+        j = json.loads(data)
+        for v in j:
+            print v
+        # Next we get the values
+        dev_id = j["id"]
+        val = j["v"]
+        # And we parse the data into a dictionary
+        datadict = {"dev_id": dev_id, "value": val}
+        # We return a dictionary of the data
+        return datadict
+    except Exception as e:
+        print e
+        return False
+
+
 def on_connect(client, userdata, flags, rc):
+    # The callback for when the client receives a CONNACK response from the server.
     print "Connected with result code "+str(rc)
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("Topic1")
 
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print msg.topic+" "+str(msg.payload)
-    # add SQL insert here
+    # Check valid json - add SQL insert here
+    data = parsedata(msg.payload)
+    if data:
+        create_reading(data["dev_id"], data["value"])
+    else:
+        print "invalid data"
+        # Implement mqtt response Here
+        return
 
 
 client = mqtt.Client()
